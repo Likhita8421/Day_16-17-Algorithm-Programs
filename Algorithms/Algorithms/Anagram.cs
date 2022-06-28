f042a31514dbbb959bf0376a3e19e39ae8d3e329
Likhita <leenupandagre24@gmail.com>
2022-06-29 00:24:49 +0530

    initial commit

---
+++ Algorithms/Algorithms/Anagram.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Algorithms
+{
+    public class Anagram
+    {
+        public bool AnagramSort(string firstString, string secondString)
+        {
+            if (firstString.Length != secondString.Length)
+            {
+                return false;
+            }
+            //Convert string to character array  
+            char[] firstCharsArray = firstString.ToLower().ToCharArray();
+            char[] secondCharsArray = secondString.ToLower().ToCharArray();
+            //Sort array  
+            Array.Sort(firstCharsArray);
+            Array.Sort(secondCharsArray);
+            //Check each character and position. 
+
+            for (int i = 0; i < firstCharsArray.Length; i++)
+            {
+                if (firstCharsArray[i].ToString() != secondCharsArray[i].ToString())
+                {
+                    return false;
+                }
+            }
+            return true;
+        }
+    }
+}
---
+++ Algorithms/Algorithms/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+namespace Algorithms
+{
+    class program
+    {
+        public static void Main(String[] args)
+        {
+            string firstString, secondString;
+            //Gets words from user.  
+            Console.WriteLine("Enter first string");
+            firstString = Console.ReadLine();
+            Console.WriteLine("Enter second string");
+            secondString = Console.ReadLine();
+            Algorithms.Anagram anagram = new Algorithms.Anagram();
+            //Check if words are anagram  
+            if (anagram.AnagramSort(firstString, secondString) == true)
+            {
+                Console.WriteLine("Both string are anagram string.");
+
+            }
+            else
+            {
+                Console.WriteLine("Both string are not anagram string.");
+
+            }
+            Console.ReadLine();
+
+        }
+    }
+}
